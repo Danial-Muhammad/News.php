9b083af16f01c082ae855e98f16d0b1591cdbde5
Danial Muhammad <ranadanialdon@gmail.com>
2020-10-15 16:17:52 +0200

    Commit Init

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rana Danial</title>
+</head>
+
+<body>
+    <div class="container">
+        This is loda lassan
+        <?php
+            echo "Danial Don hu mai";
+        ?>
+    </div>
+
+</body>
+
+</html>
